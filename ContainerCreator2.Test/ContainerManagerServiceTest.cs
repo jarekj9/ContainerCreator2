d7c17c9eef830e098331dcae9017f20c027ec580
Jaroslaw <jaroslaw.jankun@gmail.com>
2024-08-15 11:38:31 +0200

    Add few tests

--- ContainerCreator2.Test/ContainerManagerServiceTest.cs
+++ ContainerCreator2.Test/ContainerManagerServiceTest.cs
@@ -10,33 +10,26 @@ namespace ContainerCreator2.Test
 {
     public class ContainerManagerServiceTest
     {
+        private const string ContainerLifeTimeMinutes = "10";
+        private const int OneTimePasswordLength = 15;
+
         private IContainerManagerService containerManagerService;
         private Mock<IAzureAciService> azureAciServiceMock;
 
-        [SetUp]
-        public void Setup()
-        {
-            var resourceGroupName = "resourceGroupName";
-            var loggerMock = new Mock<ILogger<ContainerManagerService>>();
-            var configurationMock = new Mock<IConfiguration>();
-            configurationMock.Setup(c => c["ContainerImage"]).Returns("container-image");
-            configurationMock.Setup(c => c["ResourceGroupName"]).Returns(resourceGroupName);
-            configurationMock.Setup(c => c["ContainerPorts"]).Returns("443,80");
-            configurationMock.Setup(c => c["ContainerLifeTimeMinutes"]).Returns("10");
-            configurationMock.Setup(c => c["RegistryCredentialsServer"]).Returns("RegistryCredentialsServer");
-            configurationMock.Setup(c => c["RegistryCredentialsUserName"]).Returns("RegistryCredentialsUserName");
-            configurationMock.Setup(c => c["RegistryCredentialsPassword"]).Returns("");
-            configurationMock.Setup(c => c["TenantId"]).Returns("TenantId");
-            configurationMock.Setup(c => c["SubscriptionId"]).Returns("SubscriptionId");
+        private ContainerRequest containerRequest;
+        private ContainerInfo containerInfoReturnedByAzureAciService;
 
-            azureAciServiceMock = new Mock<IAzureAciService>();
-            containerManagerService = new ContainerManagerService(loggerMock.Object, configurationMock.Object, azureAciServiceMock.Object);
-        }
-
-        [Test]
-        public async Task TestCreateOrUpdateAsync()
+        public ContainerManagerServiceTest()
         {
-            var containerInfo = new ContainerInfo()
+            containerRequest = new ContainerRequest()
+            {
+                Id = Guid.NewGuid(),
+                OwnerId = Guid.NewGuid().ToString(),
+                DnsNameLabel = "dnsname",
+                UrlToOpenEncoded = "https%3A%2F%2Fgithub.com%2F"
+            };
+
+            containerInfoReturnedByAzureAciService = new ContainerInfo()
             {
                 Id = Guid.NewGuid(),
                 ContainerGroupName = "containerGroupName",
@@ -47,23 +40,70 @@ namespace ContainerCreator2.Test
                 CreatedTime = DateTime.UtcNow,
                 IsDeploymentSuccesful = true
             };
-            azureAciServiceMock.Setup(a => a.CreateOrUpdateAsync(It.IsAny<string>(), It.IsAny<ContainerGroupData>())).ReturnsAsync(containerInfo);
+        }
 
-            var containerRequest = new ContainerRequest()
-            {
-                Id = Guid.NewGuid(),
-                OwnerId = Guid.NewGuid().ToString(),
-                DnsNameLabel = "dnsname",
-            };
-           
+        [SetUp]
+        public void Setup()
+        {
+            var resourceGroupName = "resourceGroupName";
+            var loggerMock = new Mock<ILogger<ContainerManagerService>>();
+            var configurationMock = new Mock<IConfiguration>();
+            configurationMock.Setup(c => c["ContainerImage"]).Returns("container-image");
+            configurationMock.Setup(c => c["ResourceGroupName"]).Returns(resourceGroupName);
+            configurationMock.Setup(c => c["ContainerPorts"]).Returns("443,80");
+            configurationMock.Setup(c => c["ContainerLifeTimeMinutes"]).Returns(ContainerLifeTimeMinutes);
+            configurationMock.Setup(c => c["RegistryCredentialsServer"]).Returns("RegistryCredentialsServer");
+            configurationMock.Setup(c => c["RegistryCredentialsUserName"]).Returns("RegistryCredentialsUserName");
+            configurationMock.Setup(c => c["RegistryCredentialsPassword"]).Returns("RegistryCredentialsPassword");
+            configurationMock.Setup(c => c["TenantId"]).Returns("TenantId");
+            configurationMock.Setup(c => c["SubscriptionId"]).Returns("SubscriptionId");
+
+            azureAciServiceMock = new Mock<IAzureAciService>();
+            azureAciServiceMock.Setup(a => a.CreateOrUpdateAsync(It.IsAny<string>(), It.IsAny<ContainerGroupData>())).ReturnsAsync(containerInfoReturnedByAzureAciService);
+            containerManagerService = new ContainerManagerService(loggerMock.Object, configurationMock.Object, azureAciServiceMock.Object);
+        }
+
+        [Test]
+        public async Task CreateOrUpdateAsync_Sets_Correct_Result_Parameters()
+        {
             var containerInfoResult = await containerManagerService.CreateContainer(containerRequest);
 
             Assert.IsTrue(containerInfoResult.OwnerId.ToString() == containerRequest.OwnerId);
             Assert.IsTrue(containerInfoResult.Image == "container-image");
-            Assert.IsTrue(containerInfoResult.RandomPassword.Length == 15);
+            Assert.IsTrue(containerInfoResult.RandomPassword.Length == OneTimePasswordLength);
+        }
+
+        [Test]
+        public async Task CreateOrUpdateAsync_Sets_Basic_Container_EnvVars()
+        {
+            var containerInfoResult = await containerManagerService.CreateContainer(containerRequest);
+
             azureAciServiceMock.Verify(m => m.CreateOrUpdateAsync(
                 It.Is<string>(s => s.StartsWith("containergroup-")), 
-                It.Is<ContainerGroupData>(c => c.IPAddress.DnsNameLabel == containerRequest.DnsNameLabel)),
+                It.Is<ContainerGroupData>(
+                        c => c.IPAddress.DnsNameLabel == containerRequest.DnsNameLabel 
+                        && c.Containers.First().EnvironmentVariables.Any(e => e.Name == "MAXMINUTES" && e.Value == ContainerLifeTimeMinutes)
+                        && c.Containers.First().EnvironmentVariables.Any(e => e.Name == "URL_TO_OPEN" && e.Value == "https://github.com/")
+                        && c.Containers.First().EnvironmentVariables.Any(e => e.Name == "VNCPASS" && e.SecureValue.Length == OneTimePasswordLength)
+                    )
+                ),
+                Times.Once
+            );
+        }
+
+        [Test]
+        public async Task CreateOrUpdateAsync_Uses_Correct_Registry_Credentials()
+        {
+            var containerInfoResult = await containerManagerService.CreateContainer(containerRequest);
+
+            azureAciServiceMock.Verify(m => m.CreateOrUpdateAsync(
+                It.Is<string>(s => s.StartsWith("containergroup-")),
+                It.Is<ContainerGroupData>(
+                        c => c.ImageRegistryCredentials.First().Username == "RegistryCredentialsUserName"
+                        && c.ImageRegistryCredentials.First().Password == "RegistryCredentialsPassword"
+                        && c.ImageRegistryCredentials.First().Server == "RegistryCredentialsServer"
+                    )
+                ),
                 Times.Once
             );
         }
